d31798396c569c420be3e39853a30130be98a352
Göran Broström <goran.brostrom@umu.se>
2018-10-22 11:19:27 +0200

    new start

--- R/get_fert.R
+++ R/get_fert.R
@@ -1,26 +1,22 @@
 get_fert <- function(){
-    load("kids.rda")
-    library(skel14)
-    indx <- match(kids$ch.id, person$ID)
-    kids$mb <- person$FB[indx]
-    kids$sb <- person$DF[indx]
-    dubbel <- unique(kids$id[kids$mb > 1.5])
-    n_d <- length(dubbel)
-    fert <- kids[!(kids$id %in% dubbel), ]
-    short_ivl <- unique(fert$id[fert$exit - fert$enter < 0.76 & fert$parity > 0.5 & fert$event > 0.5])
-    fert <- fert[!(fert$id %in% short_ivl), ]
-    early <- unique(fert$id[fert$enter < 15.01])
-    fert <- fert[fert$id %in% early, ]
-    mid <- fert$id[fert$exit - fert$enter < 0.75 & fert$event > 0.5]
-    fert <- fert[!(fert$id %in% mid), ]
-    par0 <- fert$id[fert$parity == 0]
-    fert <- fert[fert$id %in% par0, ]
-    fert <- fert[fert$lopnr - fert$parity > 0.9 & fert$lopnr - fert$parity < 1.1, ]
-    
-    fert$sb <- fert$mb <- fert$lopnr <- NULL
-    fert <- fert[order(fert$id, fert$parity),]
-    rownames(fert) <- 1:NROW(fert)
-    save(fert, file = "fert.rda")
-    cat("wrote 'fert.rda' to disk")
-    invisible(fert)
+    library(skum)
+    fams <- unique(obs$id[obs$sex == "female" & obs$starttyp == 2 & obs$birthdate > 1821 & obs$birthdate < 1870])
+    women <- obs[obs$id %in% fams, c("id", "civst", "birthdate", "enter", "exit", "pid", "sluttyp")]
+    women$event <- women$sluttyp == 2
+    women <- women[order(women$id, women$enter), ]
+    women <- eha::age.window(women, c(15, 50))
+    kids <- per[per$mid %in% women$id, c("mid", "id", "kon", "foddat")]
+    names(kids) <- c("id", "ch.id", "sex", "ch.birthdate")
+    indx <- match(kids$id, per$id)
+    kids$marStart <- per$marStart[indx]
+    kids$marEnd <- per$marEnd[indx]
+    kids$birthdate <- per$foddat[indx]
+    kids <- kids[order(kids$id, kids$ch.birthdate), ]
+    ##list(women = women, kids = kids)
+    weq <- kids$id[is.na(kids$ch.birthdate)]
+    kids <- kids[!(kids$id %in% weq), ]
+    kids <- skum::rc(kids)
+    kids$civst <- "umnarried"
+    kids
+    ## To be continued!!
 }
